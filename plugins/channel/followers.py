5ea35b8d148f481036cc553e5762ab64ec89c521
Selengiks <maksiimys1980@gmail.com>
2023-07-25 19:28:21 +0300

    Freeze requirements.txt and little .gitignore update

--- plugins/channel/followers.py
+++ plugins/channel/followers.py
@@ -2,8 +2,9 @@ from loguru import logger
 import asyncio
 from support.bots import bot
 from utils.media_sync import media_files
-import followers_conf
+from plugins.channel.configs import followers_conf
 import random
+import aiogram
 
 channel_id = followers_conf.CHANNEL_ID
 update_interval = 5
@@ -12,15 +13,15 @@ last_gif = None
 
 async def track_channel_member_count(channel: str, interval: int):
     previous_member_count = await bot.get_chat_member_count(channel)
-    logger.debug(f"Previous member count: {previous_member_count}")
+    logger.debug(f"Current subscribers count: {previous_member_count}")
     while True:
         member_count = await bot.get_chat_member_count(channel)
         if member_count > previous_member_count:
-            logger.debug(member_count)  # Add normal log message
+            logger.debug(f"New subscriber. New subscribers count: {member_count}")
             await post_new_follower_media()
             previous_member_count = member_count
         elif member_count < previous_member_count:
-            logger.debug(member_count)  # Add normal log message
+            logger.debug(f"Somebody left. New subscribers count: {member_count}")
             await post_leave_follower_media()
             previous_member_count = member_count
         await asyncio.sleep(interval)
@@ -38,7 +39,7 @@ async def post_new_follower_media():
     while gif == last_gif:
         gif = random.choice(gifs)
     last_gif = gif
-    await bot.send_animation(channel_id, open(gif, "rb"))
+    # await bot.send_animation(channel_id, open(gif, "rb"))
     logger.debug("post_new_follower_gif()")
 
 
