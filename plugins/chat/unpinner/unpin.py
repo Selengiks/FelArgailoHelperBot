137ec95b1215723cd853ff2f76371e4152602805
Selengiks <maksiimys1980@gmail.com>
2023-08-28 14:28:40 +0300

    Update unpin logic if pinned message is None

--- plugins/chat/unpinner/unpin.py
+++ plugins/chat/unpinner/unpin.py
@@ -1,6 +1,19 @@
 from plugins import types, logger, asyncio
 from support.bots import bot, dp
 
+MAX_ATTEMPTS = 10
+
+
+async def get_pinned_message(chat_id, max_attempts=MAX_ATTEMPTS):
+    for i in range(1, max_attempts + 1):
+        chat = await dp.bot.get_chat(chat_id)
+        pinned_message = chat.pinned_message
+        if pinned_message is not None:
+            return pinned_message
+        logger.warning(f"Retry attempt {i}: Pinned message is {pinned_message}")
+        await asyncio.sleep(2)
+    return None
+
 
 @dp.message_handler(
     lambda message: message.from_user.id == 777000,
@@ -9,26 +22,16 @@ from support.bots import bot, dp
 async def handle_and_unpin(update: types.Update):
     chat = await dp.bot.get_chat(update.chat.id)
     channel = await dp.bot.get_chat(chat.linked_chat_id)
-    pinned_message = chat.pinned_message
 
-    is_empty = True
-    try_attempt = 10
-    while is_empty:
-        if pinned_message is None:
-            for i in range(1, try_attempt + 1):
-                await asyncio.sleep(2)
-                chat = await dp.bot.get_chat(update.chat.id)
-                pinned_message = chat.pinned_message
-                logger.warning(f"Attempt {i}: Pinned message is {pinned_message}")
-            message = (
-                f"Cannot unpin message via unexpected error, "
-                f"still get {pinned_message} after {try_attempt} attempts"
-            )
-            await bot.send_message(chat.id, message)
-            logger.warning("Message not unpinned")
-            return False
-        else:
-            is_empty = False
+    pinned_message = await get_pinned_message(chat.id)
+    if pinned_message is None:
+        message = (
+            f"Cannot unpin message via unexpected error, "
+            f"still get {pinned_message} after {MAX_ATTEMPTS} attempts"
+        )
+        await bot.send_message(chat.id, message)
+        logger.warning("Message not unpinned")
+        return False
 
     if pinned_message.from_id == channel.id:
         await dp.bot.unpin_chat_message(
